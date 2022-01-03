f835c8f6191c6a7b514e1a5c7a98bd1aff9e747f
Никита Кукушкин <95044212+Nikita41411@users.noreply.github.com>
2022-01-04 00:59:29 +0300

    new_script_work

---
+++ script1.sh
@@ -0,0 +1,8 @@
+# /bin/bash
+cd folder_3
+mkdir f_1 f_2 F_3
+cd f_1
+touch t_1.txt t_2.txt t_3.txt t_4.json t_5.json 
+mkdir f_4 f_5 F_6
+ls -la
+cp t_1.txt t_2.txt /d/hw_26/Group_26/folder_3/f_1/f_4

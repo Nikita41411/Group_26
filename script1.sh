f556dc664a0c7a15cfad48fc5b662b1899ae883e
Никита Кукушкин <95044212+Nikita41411@users.noreply.github.com>
2022-01-13 21:50:58 +0300

    add pay_1.txt

--- script1.sh
+++ script1.sh
@@ -6,4 +6,4 @@ cd f_1
 touch t_1.txt t_2.txt t_3.txt t_4.json t_5.json 
 mkdir f_4 f_5 F_6
 ls -la
-cp t_1.txt t_2.txt /d/hw_26/Group_26/f_1/f_1/f_4
+cp t_1.txt t_2.txt /d/hw_26/Terminal_linux/f_1/f_1/f_4
